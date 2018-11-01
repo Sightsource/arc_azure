bedc319ba93a3221e3f0a8827defbbb0eb1ccc76
Harper Trow <harper.trow@gmail.com>
2018-10-31 20:21:21 -0400

    added timeout in milliseconds configuration to ExAzure requests

--- lib/arc/storage/azure.ex
+++ lib/arc/storage/azure.ex
@@ -1,8 +1,9 @@
 defmodule Arc.Storage.Azure do
-  @moduledoc :false
+  @moduledoc false
 
   def put(definition, version, {file, scope}) do
     destination_dir = definition.storage_dir(version, {file, scope})
+
     case upload_file(destination_dir, file) do
       {:ok, _conn} -> {:ok, file.file_name}
       {:error, conn} -> {:error, conn}
@@ -11,29 +12,31 @@ defmodule Arc.Storage.Azure do
 
   def url(definition, version, file_and_scope, options \\ []) do
     temp_url_expires_after = Keyword.get(options, :temp_url_expires_after, default_tempurl_ttl())
-    temp_url_filename = Keyword.get(options, :temp_url_filename, :false)
-    temp_url_inline = Keyword.get(options, :temp_url_inline, :true)
+    temp_url_filename = Keyword.get(options, :temp_url_filename, false)
+    temp_url_inline = Keyword.get(options, :temp_url_inline, true)
     temp_url_method = Keyword.get(options, :temp_url_method, "GET")
+
     options =
-    Keyword.delete(options, :signed)
-    |> Keyword.merge([
-      temp_url_expires_after: temp_url_expires_after,
-      temp_url_filename: temp_url_filename,
-      temp_url_inline: temp_url_inline,
-      temp_url_method: temp_url_method
-      ]
-    )
+      Keyword.delete(options, :signed)
+      |> Keyword.merge(
+        temp_url_expires_after: temp_url_expires_after,
+        temp_url_filename: temp_url_filename,
+        temp_url_inline: temp_url_inline,
+        temp_url_method: temp_url_method
+      )
+
     build_url(definition, version, file_and_scope, options)
   end
 
-  def delete(_definition, _version, {file, :nil}) do
+  def delete(_definition, _version, {file, nil}) do
     server_object = parse_objectname_from_url(file.file_name)
-    ExAzure.request!(:delete_blob, [container(), server_object])
+    ExAzure.request!(:delete_blob, [container(), server_object, [], timeout_in_milliseconds()])
     :ok
   end
+
   def delete(definition, version, {file, scope}) do
     server_object = build_path(definition, version, {file, scope})
-    ExAzure.request!(:delete_blob, [container(), server_object])
+    ExAzure.request!(:delete_blob, [container(), server_object, [], timeout_in_milliseconds()])
     :ok
   end
 
@@ -42,7 +45,7 @@ defmodule Arc.Storage.Azure do
   end
 
   def default_tempurl_ttl() do
-    Application.get_env(:arc, :default_tempurl_ttl, (30 * 24 * 60 * 60))
+    Application.get_env(:arc, :default_tempurl_ttl, 30 * 24 * 60 * 60)
   end
 
   defp host() do
@@ -66,10 +69,20 @@ defmodule Arc.Storage.Azure do
 
   defp upload_file(destination_dir, file) do
     filename = Path.join(destination_dir, file.file_name)
-    ExAzure.request(:put_block_blob, [container(), filename, get_binary_file(file)])
+
+    ExAzure.request(:put_block_blob, [
+      container(),
+      filename,
+      get_binary_file(file),
+      [],
+      timeout_in_milliseconds()
+    ])
   end
 
   defp get_binary_file(%{path: nil} = file), do: file.binary
   defp get_binary_file(%{path: _} = file), do: File.read!(file.path)
 
+  defp timeout_in_milliseconds do
+    Application.get_env(:arc_azure, :timeout_in_milliseconds) || 30_000
+  end
 end
